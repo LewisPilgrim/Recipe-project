0e1a9e472e3320e17e589bb179f1b54753b43246
LewisPilgrim <lewis.pilgrim0@hotmail.co.uk>
2024-03-13 20:04:50 +0000

    add item logic started

--- src/app/components/ListBoxes/ShoppingList/AddItemPanel/AddItemPanel.tsx
+++ src/app/components/ListBoxes/ShoppingList/AddItemPanel/AddItemPanel.tsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
 
 const AddItemPanel = () => {
+    const [itemName, setItemName] = useState("Name")
+    const [itemAmount, setItemAmount] = useState(0)
+    const [itemAmountUnits, setItemAmountUnits] = useState("g")
+    const [ingredientList, setIngredientList] = useState<object[]>([])
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        if (e.target.id === "item-name") {
+            setItemName(e.target.value)
+        } else if (e.target.id === "item-amount") {
+            setItemAmount(Number(e.target.value))
+        }
+    }
+
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+        setItemAmountUnits(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        setIngredientList([
+            ...ingredientList,
+            {
+                name: itemName,
+                amount: itemAmount + itemAmountUnits
+            }
+        ])
+    }
+
+    useEffect(() => {
+        const createdItem = {
+            name: itemName,
+            amount: itemAmount + itemAmountUnits
+        }
+        console.log(createdItem)
+    })
+
   return (
-      <div>
-        <h2>Add a new item</h2>
-        <form>
-            <label id="item-name">Item Name</label>
-            <input value="Name" id="item-name"></input>
-        </form>
+    <div>
+      <h2>Add a new item</h2>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          <li>
+            <label id='item-name'>Item Name</label>
+            <input value={itemName} id='item-name' onChange={handleInput}></input>
+          </li>
+          <li>
+            <label id='item-amount'>Amount</label>
+            <input value={itemAmount} id='item-amount' onChange={handleInput}></input>
+          </li>
+          <li>
+            <select onChange={handleSelect}>
+              <option value='g'>g</option>
+              <option value='ml'>ml</option>
+            </select>
+          </li>
+        </ul>
+        <button type='submit'>Add</button>
+      </form>
     </div>
   )
 }
